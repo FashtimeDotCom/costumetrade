53dfb6b48149804e616b6ef4afc15b37eb8870bd
kf <kf@LAPTOP-643LK622>
2017-04-18 21:19:36 +0800

    ff

---
+++ costumetrade-permission/src/main/java/com/touchart/costumetrade/modules/costumetrade_permission/App.java
@@ -0,0 +1,13 @@
+package com.touchart.costumetrade.modules.costumetrade_permission;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
